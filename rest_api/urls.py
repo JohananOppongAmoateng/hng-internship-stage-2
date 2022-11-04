c85c3182a437a57ecb880f07434bb1deddf1bc03
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-04 15:54:53 +0000

    ftghj

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,5 +3,5 @@ from .views import api_response
 from django.urls import path
 
 urlpatterns = [
-    path("",api_response,name="jsonresponse")    
+    path("sendjson",api_response,name="jsonresponse")    
     ]
