271a04529fb579facde92d656278ba3a422c13e7
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2022-11-04 13:58:05 +0000

    Update urls.py

--- stage2/urls.py
+++ stage2/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hng-internship/stage2/',include('rest_api.urls'))
+    path('',include('rest_api.urls'))
 ]
